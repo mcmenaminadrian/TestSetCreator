fa4f0b8a2e541574d61f0ea3e974d8e9d406be04
Adrian McMenamin <adrianmcmenamin@gmail.com>
2017-07-31 17:25:01 +0100

    udaptes - not working properly

---
+++ picturedisplay.cpp
@@ -0,0 +1,7 @@
+#include "picturedisplay.hpp"
+
+PictureDisplay::PictureDisplay()
+{
+
+}
+
---
+++ picturedisplay.hpp
@@ -0,0 +1,13 @@
+#ifndef PICTUREDISPLAY_HPP
+#define PICTUREDISPLAY_HPP
+
+#include <QObject>
+#include <QWidget>
+
+class PictureDisplay : public QGraphicsView
+{
+public:
+	PictureDisplay();
+};
+
+#endif // PICTUREDISPLAY_HPP
